2081c1cc4aea3aa6ea830631cb8d88164373b405
Dplo1514 <dladlsgur3334@gmail.com>
2022-04-08 17:23:59 +0900

    TodayError Start

---
+++ src/test/java/com/example/todayError/TodayErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todayError;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayErrorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
